5709a7b7a36a48ecca92d34f72d0a1d529676cdd
DemeoStep <demeo.alex@gmail.com>
2021-10-15 20:55:52 +0300

    Homework8 completed

--- src/main/java/homework8/task2/Main.java
+++ src/main/java/homework8/task2/Main.java
@@ -2,9 +2,9 @@ package homework8.task2;
 
 public class Main {
     public static void main(String[] args) {
-        Thread main = new Thread(new SimpleThread(), "main");
-        Thread first = new Thread(new SimpleThread(), "first");
-        Thread second = new Thread(new SimpleThread(), "second");
+        Thread main = new Thread(new SimpleThread(1000), "main");
+        Thread first = new Thread(new SimpleThread(2000), "first");
+        Thread second = new Thread(new SimpleThread(3000), "second");
 
         main.start();
         first.start();
--- src/main/java/homework8/task2/SimpleThread.java
+++ src/main/java/homework8/task2/SimpleThread.java
@@ -1,16 +1,19 @@
 package homework8.task2;
 
-import java.util.Random;
+public class SimpleThread implements Runnable {
+    private int millis;
 
-public class SimpleThread implements Runnable{
+    public SimpleThread(int millis) {
+        this.millis = millis;
+    }
+
     @Override
     public void run() {
-        int rand = (new Random().nextInt(250));
         try {
-            Thread.sleep(rand);
+            Thread.sleep(millis);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("Завершение потока " + Thread.currentThread().getName() + " после задержки в " + rand + " мс");
+        System.out.println("Завершение потока " + Thread.currentThread().getName() + " после задержки в " + millis + " мс");
     }
 }
