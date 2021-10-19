388bfdc8e20b435e4d829d2e4683f5b394fe824c
DemeoStep <demeo.alex@gmail.com>
2021-10-19 14:53:19 +0300

    Homework9 completed

---
+++ src/main/java/homework9/task3_tryLock/ClassA.java
@@ -0,0 +1,38 @@
+package homework9.task3_tryLock;
+
+public class ClassA implements Runnable {
+    private final Counter counter;
+    private Thread thread;
+
+    ClassA(Counter counter) {
+        this.counter = counter;
+    }
+
+    public void setThread(Thread thread) {
+        this.thread = thread;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("ClassA thread start");
+        for (int i = 0; i < 1000; i++) {
+            if (counter.tryLock()) {
+                System.out.println("ClassA tick");
+                counter.tick();
+                counter.unlock();
+                try {
+                    Thread.sleep(0, 10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                i--;
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/homework9/task3_tryLock/Counter.java
@@ -0,0 +1,26 @@
+package homework9.task3_tryLock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Counter {
+    private int counter;
+    private final Lock lock = new ReentrantLock();
+
+    public boolean tryLock() {
+        return lock.tryLock();
+    }
+
+    public void unlock() {
+        lock.unlock();
+    }
+
+    public void tick() {
+        this.counter++;
+    }
+
+    @Override
+    public String toString() {
+        return "counter = " + counter;
+    }
+}
